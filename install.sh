01c68dc2d5f991a1a83606244b91ce0fcd487a6a
Joshua Yuter <jyuter@gmail.com>
2024-07-28 00:29:21 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -118,14 +118,6 @@ apt install npm -y
 npm install -g pnpm
 npm install -g bun
 
-#VsCode
-apt-get install wget gpg -y
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-rm -f packages.microsoft.gpg
-apt install apt-transport-https -y
-apt update
 
 #Docker 
 sysctl -w kernel.apparmor_restrict_unprivileged_userns=0 
